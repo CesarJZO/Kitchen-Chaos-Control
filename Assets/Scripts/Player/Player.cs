b8b8481e726030c4a1a3aca1fc4a4955e277ee6b
César Zárate <cesarjzo@outlook.com>
2023-02-18 23:23:30 -0600

    Add diagonal collision polish

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -21,7 +21,32 @@ namespace CodeMonkey.KitchenCaosControl.Player
             var position = t.position;
 
             var moveDistance = moveSpeed * Time.deltaTime;
-            var canMove = !Physics.CapsuleCast(position, position + Vector3.up, playerRadius, moveDirection, moveDistance);
+            var canMove = !CapsuleCast(moveDirection);
+
+            if (!canMove)
+            {
+                // Cannot move towards the direction, try to move sideways
+
+                // Attempt only x movement
+                var moveDirectionX = new Vector3(moveDirection.x, 0f, 0f).normalized;
+                canMove = !CapsuleCast(moveDirectionX);
+
+                if (canMove)
+                {
+                    // Can move only on the x
+                    moveDirection = moveDirectionX;
+                }
+                else
+                {
+                    // Attempt only z movement
+                    var moveDirectionZ = new Vector3(0f, 0f, moveDirection.z).normalized;
+                    canMove = !CapsuleCast(moveDirectionZ);
+                    if (canMove) // Can move only on the z
+                        moveDirection = moveDirectionZ;
+                    // else cannot move at all
+                }
+            }
+
             IsWalking = moveDirection != Vector3.zero && canMove;
 
             if (canMove)
@@ -29,6 +54,8 @@ namespace CodeMonkey.KitchenCaosControl.Player
 
             var forward = Vector3.Slerp(t.forward, moveDirection, Time.deltaTime * rotationSpeed);
             if (forward != Vector3.zero) t.forward = forward;
+
+            bool CapsuleCast(Vector3 direction) => Physics.CapsuleCast(position, position + Vector3.up, playerRadius, direction, moveDistance);
         }
     }
 }
