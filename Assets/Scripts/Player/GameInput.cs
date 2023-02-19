9f1f90a363be7794d62876225a3f9dbf8ac3a280
César Zárate <cesarjzo@outlook.com>
2023-02-18 23:04:42 -0600

    Add simple collisions

--- Assets/Scripts/Player/GameInput.cs
+++ Assets/Scripts/Player/GameInput.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using CodeMonkey.KitchenCaosControl.Input;
+﻿using CodeMonkey.KitchenCaosControl.Input;
 using UnityEngine;
 
 namespace CodeMonkey.KitchenCaosControl
--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,8 +4,10 @@ namespace CodeMonkey.KitchenCaosControl.Player
 {
     public class Player : MonoBehaviour
     {
+        [Header("Settings")]
         [SerializeField] private float moveSpeed;
         [SerializeField] private float rotationSpeed;
+        [SerializeField] private float playerRadius;
 
         [Header("Dependencies")]
         [SerializeField] private GameInput gameInput;
@@ -16,8 +18,14 @@ namespace CodeMonkey.KitchenCaosControl.Player
         {
             var moveDirection = gameInput.GetNormalizedMovementVector();
             var t = transform;
-            t.position += moveDirection * (moveSpeed * Time.deltaTime);
-            IsWalking = moveDirection != Vector3.zero;
+            var position = t.position;
+
+            var moveDistance = moveSpeed * Time.deltaTime;
+            var canMove = !Physics.CapsuleCast(position, position + Vector3.up, playerRadius, moveDirection, moveDistance);
+            IsWalking = moveDirection != Vector3.zero && canMove;
+
+            if (canMove)
+                t.position += moveDirection * moveDistance;
 
             var forward = Vector3.Slerp(t.forward, moveDirection, Time.deltaTime * rotationSpeed);
             if (forward != Vector3.zero) t.forward = forward;
