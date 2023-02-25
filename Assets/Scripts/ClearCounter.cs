ec0bceee6fee700b2ec1d27050a3ab575cf7301b
César Zárate <cesarjzo@outlook.com>
2023-02-25 15:03:04 -0600

    Add test key, success

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,5 +1,6 @@
 ﻿using CodeMonkey.KitchenCaosControl.ScriptableObjects;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace CodeMonkey.KitchenCaosControl
 {
@@ -7,6 +8,10 @@ namespace CodeMonkey.KitchenCaosControl
     {
         [SerializeField] private KitchenScriptableObject kitchenScriptableObject;
         [SerializeField] private Transform counterTopPoint;
+
+        [Header("Testing")]
+        [SerializeField] private bool testing;
+        [SerializeField] private ClearCounter secondClearCounter;
         public Transform CounterTopPoint => counterTopPoint;
 
         private KitchenObjectBehaviour _currentKitchenObject;
@@ -17,6 +22,14 @@ namespace CodeMonkey.KitchenCaosControl
             set => _currentKitchenObject = value;
         }
 
+        private void Update()
+        {
+            if (!testing || !Keyboard.current.tKey.wasPressedThisFrame) return;
+
+            if (KitchenObject)
+                KitchenObject.ClearCounter = secondClearCounter;
+        }
+
         public void ClearKitchenObject() => _currentKitchenObject = null;
 
         public bool HasKitchenObject => _currentKitchenObject;
@@ -24,7 +37,7 @@ namespace CodeMonkey.KitchenCaosControl
         public void Interact()
         {
             // If there is no current kitchen object, add one
-            if (_currentKitchenObject) return;
+            if (KitchenObject) return;
             var kitchenObject = Instantiate(kitchenScriptableObject.Prefab, counterTopPoint);
             /* There's no need to set the current kitchen object's clear counter to this, because the kitchen object's
                 clear counter will be set by the kitchen object */
