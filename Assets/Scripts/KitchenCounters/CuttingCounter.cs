74d09176dcd0e9bb936156987b08924f3eba8f7b
César Zárate <cesarjzo@outlook.com>
2023-02-26 12:54:29 -0600

    Add CuttingCounter prefab and script. Add walls to GameScene.unity

---
+++ Assets/Scripts/KitchenCounters/CuttingCounter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace CodeMonkey.KitchenCaosControl.KitchenCounters
+{
+    public class CuttingCounter : BaseCounter
+    {
+        public override void Interact(Player player)
+        {
+            if (HasKitchenObject())
+                GetKitchenObject().SetAndTeleportToParent(player);
+            else if (player.HasKitchenObject())
+                player.GetKitchenObject().SetAndTeleportToParent(this);
+        }
+    }
+}
