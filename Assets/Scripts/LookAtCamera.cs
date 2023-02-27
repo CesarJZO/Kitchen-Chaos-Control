eb962d0f9db0027f4e793cfb7e715d8c74cb1db2
César Zárate <cesarjzo@outlook.com>
2023-02-26 21:41:45 -0600

    Add LookAtCamera.cs script

---
+++ Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+
+namespace CodeMonkey.KitchenCaosControl
+{
+    public class LookAtCamera : MonoBehaviour
+    {
+        private enum Mode
+        {
+            LookAt,
+            LookAtInverted,
+            CameraForward,
+            CameraForwardInverted
+        }
+
+        [SerializeField] private Mode mode;
+
+        private Camera _camera;
+
+        private void Awake()
+        {
+            _camera = Camera.main;
+        }
+
+        private void LateUpdate()
+        {
+            switch (mode)
+            {
+                case Mode.LookAt:
+                    transform.LookAt(_camera.transform);
+                    break;
+                case Mode.LookAtInverted:
+                    var position = transform.position;
+                    var directionFromCamera = position - _camera.transform.position;
+                    transform.LookAt(position + directionFromCamera);
+                    break;
+                case Mode.CameraForward:
+                    transform.forward = _camera.transform.forward;
+                    break;
+                case Mode.CameraForwardInverted:
+                    transform.forward = -_camera.transform.forward;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+}
