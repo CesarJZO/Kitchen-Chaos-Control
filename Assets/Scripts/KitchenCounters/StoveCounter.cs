46b0ec7666f4281e1d05cad7000260b78321c484
César Zárate <cesarjzo@outlook.com>
2023-03-04 19:24:54 -0600

    Add StoveCounter.cs state machine

--- Assets/Scripts/KitchenCounters/StoveCounter.cs
+++ Assets/Scripts/KitchenCounters/StoveCounter.cs
@@ -7,33 +7,75 @@ namespace CodeMonkey.KitchenCaosControl.KitchenCounters
 {
     public class StoveCounter : Counter
     {
-        [SerializeField] private FryingRecipe[] fryingRecipes;
+        private enum State
+        {
+            Idle,
+            Frying,
+            Fried,
+            Burned
+        }
 
+        [SerializeField] private FryingRecipe[] fryingRecipes;
+        [SerializeField] private BurningRecipe[] burningRecipes;
+
+        private State _state;
         private float _fryingTimer;
         private FryingRecipe _currentFryingRecipe;
+        private float _burningTimer;
+        private BurningRecipe _currentBurningRecipe;
+
+        private void Start()
+        {
+            _state = State.Idle;
+        }
 
         private void Update()
         {
-            if (HasKitchenObject())
+            if (!HasKitchenObject()) return;
+
+            switch (_state)
             {
-                _fryingTimer += Time.deltaTime;
-                if (_fryingTimer >= _currentFryingRecipe.FryingMaxTime)
-                {
-                    _fryingTimer = 0f;
+                case State.Idle: break;
+                case State.Frying:
+                    _fryingTimer += Time.deltaTime;
+                    if (_fryingTimer < _currentFryingRecipe.FryingMaxTime) return;
+
                     GetKitchenObject().DestroySelf();
                     KitchenObject.SpawnKitchenObject(_currentFryingRecipe.Output, this);
-                }
+                    _burningTimer = 0f;
+
+                    _currentBurningRecipe = GetBurningRecipeWithInput(GetKitchenObject().Data);
+
+                    _state = State.Fried;
+                    break;
+                case State.Fried:
+                    _burningTimer += Time.deltaTime;
+                    if (_burningTimer < _currentBurningRecipe.BurningMaxTime) return;
+
+                    GetKitchenObject().DestroySelf();
+                    KitchenObject.SpawnKitchenObject(_currentBurningRecipe.Output, this);
+                    _burningTimer = 0f;
+                    _state = State.Burned;
+                    break;
+                case State.Burned: break;
+                default: throw new ArgumentOutOfRangeException();
             }
         }
 
         public override void Interact(Player player)
         {
             if (HasKitchenObject())
+            {
+                if (!player.HasKitchenObject())
+                    _state = State.Idle;
                 GetKitchenObject().SetAndTeleportToParent(player);
+            }
             else if (player.HasKitchenObject() && HasRecipe(player.GetKitchenObject()))
             {
                 player.GetKitchenObject().SetAndTeleportToParent(this);
-                _currentFryingRecipe = GetRecipeWithInput(GetKitchenObject().Data);
+                _currentFryingRecipe = GetFryingRecipeWithInput(GetKitchenObject().Data);
+                _state = State.Frying;
+                _fryingTimer = 0f;
             }
 
             bool HasRecipe(KitchenObject input)
@@ -43,24 +85,29 @@ namespace CodeMonkey.KitchenCaosControl.KitchenCounters
         }
 
         /// <summary>
-        /// Returns the KitchenObjectData for the given input, or null if there is none
-        /// </summary>
-        /// <param name="input">If it is referenced as input in a recipe,returns its defined
-        /// output KitchenObjectData. Null if don't have a match</param>
-        private KitchenObjectData GetOutputForInput(KitchenObjectData input)
-        {
-            return GetRecipeWithInput(input)?.Output;
-        }
-
-        /// <summary>
-        /// Returns the cutting recipe for the given input, or null if there is none
+        /// Returns the frying recipe for the given input, or null if there is none
         /// </summary>
         /// <param name="input">If it is referenced as input in a recipe, returns the recipe.
         /// Null if there is no match</param>
-        private FryingRecipe GetRecipeWithInput(KitchenObjectData input)
+        private FryingRecipe GetFryingRecipeWithInput(KitchenObjectData input)
         {
             // Select the possible output of the cutting recipe
             var slicedCandidates = from cuttingRecipe in fryingRecipes
+                where cuttingRecipe.Input == input
+                select cuttingRecipe;
+            // Get the first possible output, or null if there is none
+            return slicedCandidates.FirstOrDefault();
+        }
+
+        /// <summary>
+        /// Returns the burning recipe for the given input, or null if there is none
+        /// </summary>
+        /// <param name="input">If it is referenced as input in a recipe, returns the recipe.
+        /// Null if there is no match</param>
+        private BurningRecipe GetBurningRecipeWithInput(KitchenObjectData input)
+        {
+            // Select the possible output of the cutting recipe
+            var slicedCandidates = from cuttingRecipe in burningRecipes
                 where cuttingRecipe.Input == input
                 select cuttingRecipe;
             // Get the first possible output, or null if there is none
---
+++ Assets/Scripts/ScriptableObjects/BurningRecipe.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace CodeMonkey.KitchenCaosControl.ScriptableObjects
+{
+    [CreateAssetMenu]
+    public class BurningRecipe : ScriptableObject
+    {
+        [field:SerializeField] public KitchenObjectData Input { get; private set; }
+        [field:SerializeField] public KitchenObjectData Output { get; private set; }
+        [field:SerializeField] public float BurningMaxTime { get; private set; }
+    }
+}
