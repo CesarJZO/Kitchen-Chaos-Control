ab27abbc3885d4d7186842549fd02cb6280d79b7
César Zárate <cesarjzo@outlook.com>
2023-03-12 15:06:42 -0600

    Add DeliveryCounter.cs

---
+++ Assets/Scripts/KitchenCounters/DeliveryCounter.cs
@@ -0,0 +1,13 @@
+﻿namespace CodeMonkey.KitchenCaosControl.KitchenCounters
+{
+    public class DeliveryCounter : Counter
+    {
+        public override void Interact(Player player)
+        {
+            if (!player.HasKitchenObject()) return;
+
+            if (player.GetKitchenObject().TryGetPlate(out _))
+                player.GetKitchenObject().DestroySelf();
+        }
+    }
+}
